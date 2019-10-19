a295b658f140e62452afa7606a85143793dd5e1d
gianlopes <54591501+gianlopes@users.noreply.github.com>
2019-10-19 16:54:10 -0300

    Implement the other functions Fixed some regex bugs

--- src/com/ricardo/Controller.java
+++ src/com/ricardo/Controller.java
@@ -8,7 +8,15 @@ class Controller {
     //Run function
     void run(){
         System.out.println("run() called.");
-        theOne.bind(data -> myFunctions.readFile()).bind(myFunctions::filterChars).bind(myFunctions::normalize).bind2(myFunctions::scan).bind3(myFunctions::removeStopWords).printMe();
+        theOne.bind(data -> myFunctions.readFile())
+                .bind(myFunctions::filterChars)
+                .bind(myFunctions::normalize)
+                .bind2(myFunctions::scan)
+                .bind3(myFunctions::removeStopWords)
+                .bind4(myFunctions::frequencies)
+                .bind5(myFunctions::sort)
+                .bind6(myFunctions::top_25_freqs)
+                .printMe();
         System.out.println("run() ended.");
     }
 }
--- src/com/ricardo/TheOne.java
+++ src/com/ricardo/TheOne.java
@@ -1,35 +1,69 @@
 package com.ricardo;
 
-import java.util.Arrays;
-import java.util.Vector;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 class TheOne {
     private String value;
     private String[] words;
+    private Map<String, MutableInteger> wordFreqHash;
+    private List<WordFrequencyPair> wordFreqPairs;
+    private List<String> wordFreq;
 
     TheOne(String value) {
         this.value = value;
     }
 
-    TheOne bind(Function<String, String> myFunction){
+    TheOne bind(Function<String, String> myFunction) {
         this.value = myFunction.apply(this.value);
         return this;
     }
 
-    TheOne bind2(Function<String, String[]> myFunc){
+    TheOne bind2(Function<String, String[]> myFunc) {
         this.words = myFunc.apply(this.value);
         return this;
     }
 
-    TheOne bind3(Function<String[], String[]> myFun){
+    TheOne bind3(Function<String[], String[]> myFun) {
         this.words = myFun.apply(this.words);
         return this;
     }
 
-    void printMe(){
-        for (String w:words) {
+    TheOne bind4(Function<String[], Map<String, MutableInteger>> myFunc) {
+        this.wordFreqHash = myFunc.apply(this.words);
+        return this;
+    }
+
+    TheOne bind5(Function<Map<String, MutableInteger>, List<WordFrequencyPair>> myFunc) {
+        this.wordFreqPairs = myFunc.apply(this.wordFreqHash);
+        return this;
+    }
+
+    TheOne bind6(Function<List<WordFrequencyPair>, List<String>> myFunc){
+        this.wordFreq = myFunc.apply(this.wordFreqPairs);
+        return this;
+    }
+
+    //Prints list of 25 most used words with sorted freqs
+    void printMe() {
+        for (String str : wordFreq){
+            System.out.println(str);
+        }
+    }
+
+    //TODO delete these other functions (could be useful)
+    void printMe1() {
+        for (String w : words) {
             System.out.println(w);
         }
     }
+
+    //Prints hashmap
+    void printMe2() {
+        for (Map.Entry<String, MutableInteger> entry : wordFreqHash.entrySet()) {
+            System.out.println(entry.getKey() + " ==> " + entry.getValue().getValue());
+        }
+    }
 }
+
