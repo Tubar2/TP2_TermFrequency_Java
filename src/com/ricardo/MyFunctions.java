d12811157f00ada1643abfa190d42a0aef85ea77
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-19 07:53:26 -0300

    Create removeStopWords function

--- src/com/ricardo/MyFunctions.java
+++ src/com/ricardo/MyFunctions.java
@@ -3,6 +3,9 @@ package com.ricardo;
 import javax.print.DocFlavor;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 class MyFunctions {
@@ -46,7 +49,38 @@ class MyFunctions {
 
     String[] removeStopWords(String[] words){
 
+        StringBuilder t_stop_words = new StringBuilder();
 
+        Scanner scanner = null;
+
+        //Create String of stop words -> stop_words
+        try{
+            scanner = new Scanner(new FileReader("stops.txt"));
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                System.out.println("Read " + line);
+                t_stop_words.append(line);
+                t_stop_words.append(" ");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if(scanner != null){
+                scanner.close();
+            }
+        }
+
+        //Create String vector of original words
+        String e_stop_words = t_stop_words.toString();
+        String[] stop_words = e_stop_words.split(" ");
+        List<String> list = new ArrayList<String>(Arrays.asList(words));
+
+        for (String stop_word:stop_words) {
+            list.remove(stop_word);
+        }
+
+        words = list.toArray(new String[0]);
         return words;
     }
 }
