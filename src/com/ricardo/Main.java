70c760d44f8131547c8075afc4fb649c7846d0d1
Ricardo Santos <rick.giometti@hotmail.com>
2019-10-27 11:12:43 -0300

    Insert timeStamp to check program runtime

--- src/com/ricardo/Main.java
+++ src/com/ricardo/Main.java
@@ -1,9 +1,12 @@
 package com.ricardo;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
 
         String path;
         if (args.length > 0) {
@@ -22,6 +25,11 @@ public class Main {
                 .bind(MyFunctions::sort)
                 .bind(MyFunctions::top_25_freqs)
                 .bind(TheOne::printMe);
+
+        long endTime = System.nanoTime();
+        long totalTime = TimeUnit.NANOSECONDS.toMillis((endTime - startTime));
+
+        System.out.println("Total time = " + totalTime + " milliseconds");
 
     }
 
